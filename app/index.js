4227415ba2d32240bb9df914358100310c0231bc
leongaban <leongaban@gmail.com>
2017-05-06 10:34:45 -0500

    index.js created and devDeps installed

---
+++ app/index.js
@@ -0,0 +1,22 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('./index.css');
+
+// state
+// lifecycle event
+// UI
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				Hell World!
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app');
+);
