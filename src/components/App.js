c2e6a4c8fdc0dd02cd8a60df6a7eda7df19692d2
leongaban <leongaban@gmail.com>
2017-06-01 14:02:37 -0500

    Results component created and api updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,21 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Home'
 import Battle from './Battle'
+import Results from './Results'
 import Popular from './Popular'
 import Nav from './Nav'
 
 export default class App extends React.Component {
+
 	render() {
 		return (
 			<Router>
 				<div className="container">
 					<Nav />
 					<Switch>
-						<Route path="/" exact component={ Home } />
-						<Route path="/battle" exact component={ Battle } />
+						<Route exact path="/" component={ Home } />
+						<Route exact path="/battle" component={ Battle } />
+						<Route path="/battle/results" exact component={ Results } />
 						<Route path="/popular" component={ Popular } />
 						<Route render={ function() {
 							return <p>Not Found</p>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,60 @@
 import axios from 'axios'
 
-const errLog = (method, err) => console.log(`%c${method}`, 'background: #393939; color: #F25A43', err);
+const id = 'Iv1.bbe467a8ce6560db';
+const sec = '860876aa00f6e77277af52d68b054664a056b572';
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+const errlog = (method, err) => console.warn(`%c${method}`, 'background: #393939; color: #F25A43', err);
+
+function getProfile(username) {
+	return axios.get(`https://api.github.com/users/${username}${params}`)
+		.then((user) => user.data)
+		.catch((err) => errlog('api.getProfile', err));
+}
+
+function getRepos(username) {
+	return axious.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+		.catch((err) => errlog('api.getRepos', err));
+}
+
+function getStarCount(repos) {
+	return repos.data.reduce((count, repo) => count + repo.stargazers_count, 0);
+}
+
+function calculateScore(profile, repos) {
+	const followers = profile.followers;
+	const totalStars = getStarCount(repos);
+	return (followers * 3) + totalStars;
+}
+
+function getUserData(player) {
+	return axios.all([
+		getProfile(player),
+		getRepos(player)
+	]).then((data) => {
+		const profile = data[0];
+		const repos   = data[1];
+		return {
+			profile,
+			score: calculateScore(profile, repos)
+		}
+	}).catch((err) => errlog('api.fetchPopularRepos', err));
+}
+
+function sortPlayers(players) {
+	return players.sort((a, b) => b.score - a.score);
+}
+
+export const battle = (players) => {
+	return axios.all(players.map(getUserData))
+		.then(sortPlayers)
+		.catch((err) => errlog('api.battle', err));
+};
 
 export const getPopularRepos = (language) => {
 	const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
 
 	return axios.get(encodedURI)
 		.then((res) => res.data.items)
-		.catch((err) => errLog('api.fetchPopularRepos', err));
+		.catch((err) => errlog('api.fetchPopularRepos', err));
 };
