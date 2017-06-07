d9928c43874e052c8f6bc2b74290214c312846c8
leongaban <leongaban@gmail.com>
2017-06-07 17:52:52 -0500

    firebase deploy script added

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 
-module.exports = {
+const config = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -29,3 +30,16 @@ module.exports = {
 		new CopyWebpackPlugin([{ from: 'src/static' }])
 	]
 }
+
+if (process.env.NODE_ENV === 'production') {
+	config.plugins.push(
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin()
+	)
+}
+
+module.exports = config;
